8d3e6ceaf58dfe3fa26428c40b15d33ef72993f4
Peter Fagerkvist <p.fagerkvist@gmail.com>
2020-12-08 22:46:14 +0100

    auth

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const connectDB = require("./config/db");
 
-app.get('/', (req, res) => res.send('Api Running'))
-app.listen(port, () => console.log(`Server app listening on port ${port}!`))
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Api Running"));
+app.listen(port, () => console.log(`Server app listening on port ${port}!`));
